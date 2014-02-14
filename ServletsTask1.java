82c4a0d8b113ac3c696534d939ce9c495dada1e4
PinkJazz <smoothpinkjazz@gmail.com>
2014-02-13 18:25:59 -0700

    Initial source code

---
+++ ServletsTask1.java
@@ -0,0 +1,30 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class ServletsTask1 extends HttpServlet {
+
+	private static final long serialVersionUID = -3764767856748158770L;
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("<html>");
+		out.println("<head><title>Lab 3 Part 1</title></head>");
+		out.println("<body><form method=\"post\">");
+		out.println("<h2>Your name</h2>");
+		out.println("First name: <input type=\"text\" name=\"firstname\"><br>");
+		out.println("Last name: <input type=\"text\" name=\"lastname\">");
+		out.println("<h2>Programming languages you know");
+		out.println("<input type=\"checkbox\" name=\"java\">Java<br>");
+		out.println("<input type=\"checkbox\" name=\"c\">C<br>");
+		out.println("<input type=\"checkbox\" name=\"cpp\">C++<br>");
+		out.println("<input type=\"checkbox\" name=\"csharp\">C#<br>");
+		
+		
+		
+	}
+	
+}
