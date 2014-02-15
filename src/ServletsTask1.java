8af399025d66df8e760f2f55e881c5ea40818a3b
PinkJazz <smoothpinkjazz@gmail.com>
2014-02-15 12:35:35 -0700

    completed form

--- src/ServletsTask1.java
+++ src/ServletsTask1.java
@@ -1,6 +1,3 @@
-
-package src;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -25,7 +22,7 @@ public class ServletsTask1 extends HttpServlet
 		out.println("<h2>Your name</h2>");
 		out.println("First name: <input type=\"text\" name=\"firstname\"><br>");
 		out.println("Last name: <input type=\"text\" name=\"lastname\">");
-		out.println("<h2>Programming languages you know");
+		out.println("<h2>Programming languages you know</h2>");
 		out.println("<input type=\"checkbox\" name=\"java\">Java<br>");
 		out.println("<input type=\"checkbox\" name=\"c\">C<br>");
 		out.println("<input type=\"checkbox\" name=\"cpp\">C++<br>");
@@ -37,6 +34,30 @@ public class ServletsTask1 extends HttpServlet
 		out.println("<input type=\"checkbox\" name=\"scala\">Scala<br>");
 		out.println("<input type=\"checkbox\" name=\"scheme\">Scheme<br>");
 		out.println("<input type=\"checkbox\" name=\"prolog\">Prolog<br>");
-		out.println("<input type=\"checkbox\" name=\"other\">Other<br>");
+		out.println("<input type=\"checkbox\" name=\"otherlang\">Other");
+		out.println("<h2>Days of the week you can meet</h2>");
+		out.println("<input type=\"checkbox\" name=\"sun\">Sunday<br>");
+		out.println("<input type=\"checkbox\" name=\"mon\">Monday<br>");
+		out.println("<input type=\"checkbox\" name=\"tue\">Tuesday<br>");
+		out.println("<input type=\"checkbox\" name=\"wed\">Wednesday<br>");
+		out.println("<input type=\"checkbox\" name=\"thu\">Thursday<br>");
+		out.println("<input type=\"checkbox\" name=\"fri\">Friday<br>");
+		out.println("<input type=\"checkbox\" name=\"sat\">Saturday");
+		out.println("<h2>Your favorite color</h2>");
+		out.println("<input type=\"radio\" name=\"red\">Red<br>");
+		out.println("<input type=\"radio\" name=\"blue\">Blue<br>");
+		out.println("<input type=\"radio\" name=\"green\">Green<br>");
+		out.println("<input type=\"radio\" name=\"yellow\">Yellow<br>");
+		out.println("<input type=\"radio\" name=\"orange\">Orange<br>");
+		out.println("<input type=\"radio\" name=\"purple\">Purple<br>");
+		out.println("<input type=\"radio\" name=\"pink\">Pink<br>");
+		out.println("<input type=\"radio\" name=\"brown\">Brown<br>");
+		out.println("<input type=\"radio\" name=\"black\">Black<br>");
+		out.println("<input type=\"radio\" name=\"white\">White<br>");
+		out.println("<input type=\"radio\" name=\"gray\">Gray<br>");
+		out.println("<input type=\"radio\" name=\"othercolor\">Other");
+		out.println("<input type=\"submit\" value=\"Submit\">");
+		out.println("</form></body>");
+		out.println("</html>");
 	}
 }
