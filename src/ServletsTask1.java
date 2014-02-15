a2512cd6b8d3369a5e0726a085ba97213ff61824
PinkJazz <smoothpinkjazz@gmail.com>
2014-02-14 18:57:17 -0700

    added more languages

--- src/ServletsTask1.java
+++ src/ServletsTask1.java
@@ -1,3 +1,6 @@
+
+package src;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,9 +14,9 @@ public class ServletsTask1 extends HttpServlet
 
 	private static final long	serialVersionUID	= -3764767856748158770L;
 
+	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
-
 		res.setContentType("text/html");
 		PrintWriter out= res.getWriter();
 		out.println("<html>");
@@ -26,6 +29,14 @@ public class ServletsTask1 extends HttpServlet
 		out.println("<input type=\"checkbox\" name=\"java\">Java<br>");
 		out.println("<input type=\"checkbox\" name=\"c\">C<br>");
 		out.println("<input type=\"checkbox\" name=\"cpp\">C++<br>");
+		out.println("<input type=\"checkbox\" name=\"objc\">Objective-C<br>");
 		out.println("<input type=\"checkbox\" name=\"csharp\">C#<br>");
+		out.println("<input type=\"checkbox\" name=\"php\">PHP<br>");
+		out.println("<input type=\"checkbox\" name=\"perl\">Perl<br>");
+		out.println("<input type=\"checkbox\" name=\"python\">Python<br>");
+		out.println("<input type=\"checkbox\" name=\"scala\">Scala<br>");
+		out.println("<input type=\"checkbox\" name=\"scheme\">Scheme<br>");
+		out.println("<input type=\"checkbox\" name=\"prolog\">Prolog<br>");
+		out.println("<input type=\"checkbox\" name=\"other\">Other<br>");
 	}
 }
