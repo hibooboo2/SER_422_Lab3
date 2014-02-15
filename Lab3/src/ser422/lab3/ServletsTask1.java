26d13f2ab28ceb691457f3ec8ec1d0a0670c62ce
James Harris <wizardofmath@gmail.com>
2014-02-15 14:42:21 -0700

    Got it to get!

--- Lab3/src/ser422/lab3/ServletsTask1.java
+++ Lab3/src/ser422/lab3/ServletsTask1.java
@@ -41,6 +41,60 @@ public class ServletsTask1 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		try
+		{
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head><title>Lab 3 Part 1</title></head>");
+			out.println("<body bgcolor=\"pink\"><form method=\"post\">");
+			out.println("<h2>Your name</h2>");
+			out.println("First name: <input type=\"text\" name=\"firstname\"><br>");
+			out.println("Last name: <input type=\"text\" name=\"lastname\">");
+			out.println("<h2>Programming languages you know</h2>");
+			out.println("<input type=\"checkbox\" name=\"java\">Java<br>");
+			out.println("<input type=\"checkbox\" name=\"c\">C<br>");
+			out.println("<input type=\"checkbox\" name=\"cpp\">C++<br>");
+			out.println("<input type=\"checkbox\" name=\"objc\">Objective-C<br>");
+			out.println("<input type=\"checkbox\" name=\"csharp\">C#<br>");
+			out.println("<input type=\"checkbox\" name=\"php\">PHP<br>");
+			out.println("<input type=\"checkbox\" name=\"perl\">Perl<br>");
+			out.println("<input type=\"checkbox\" name=\"python\">Python<br>");
+			out.println("<input type=\"checkbox\" name=\"js\">JavaScript<br>");
+			out.println("<input type=\"checkbox\" name=\"scala\">Scala<br>");
+			out.println("<input type=\"checkbox\" name=\"scheme\">Scheme<br>");
+			out.println("<input type=\"checkbox\" name=\"prolog\">Prolog<br>");
+			out.println("<input type=\"checkbox\" name=\"otherlang\">Other");
+			out.println("<h2>Days of the week you can meet</h2>");
+			out.println("<input type=\"checkbox\" name=\"sun\">Sunday<br>");
+			out.println("<input type=\"checkbox\" name=\"mon\">Monday<br>");
+			out.println("<input type=\"checkbox\" name=\"tue\">Tuesday<br>");
+			out.println("<input type=\"checkbox\" name=\"wed\">Wednesday<br>");
+			out.println("<input type=\"checkbox\" name=\"thu\">Thursday<br>");
+			out.println("<input type=\"checkbox\" name=\"fri\">Friday<br>");
+			out.println("<input type=\"checkbox\" name=\"sat\">Saturday");
+			out.println("<h2>Your favorite color</h2>");
+			out.println("<input type=\"radio\" name=\"red\">Red<br>");
+			out.println("<input type=\"radio\" name=\"blue\">Blue<br>");
+			out.println("<input type=\"radio\" name=\"green\">Green<br>");
+			out.println("<input type=\"radio\" name=\"yellow\">Yellow<br>");
+			out.println("<input type=\"radio\" name=\"orange\">Orange<br>");
+			out.println("<input type=\"radio\" name=\"purple\">Purple<br>");
+			out.println("<input type=\"radio\" name=\"pink\">Pink<br>");
+			out.println("<input type=\"radio\" name=\"brown\">Brown<br>");
+			out.println("<input type=\"radio\" name=\"black\">Black<br>");
+			out.println("<input type=\"radio\" name=\"white\">White<br>");
+			out.println("<input type=\"radio\" name=\"gray\">Gray<br>");
+			out.println("<input type=\"radio\" name=\"othercolor\">Other<br>");
+			out.println("<input type=\"submit\" value=\"Submit\">");
+			out.println("</form></body>");
+			out.println("</html>");
+		}
+		finally
+		{
+			out.close();
+		}
 	}
 
 	/**
@@ -62,6 +116,7 @@ public class ServletsTask1 extends HttpServlet {
 			out.println("<head><title>Lab 3 Part 1</title></head>");
 			out.println("<body bgcolor=\"pink\"><form method=\"post\">");
 			out.println("<h2>Your name</h2>");
+			out.println("POST~!<BR>");
 			out.println("First name: <input type=\"text\" name=\"firstname\"><br>");
 			out.println("Last name: <input type=\"text\" name=\"lastname\">");
 			out.println("<h2>Programming languages you know</h2>");
