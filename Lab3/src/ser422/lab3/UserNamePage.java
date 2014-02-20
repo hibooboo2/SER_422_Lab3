623a19029cc3aad5a36b1a2b98561b997c75e352
James Harris <wizardofmath@gmail.com>
2014-02-20 14:43:49 -0700

    Changed username page mapping

--- Lab3/src/ser422/lab3/UserNamePage.java
+++ Lab3/src/ser422/lab3/UserNamePage.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class firstName
  */
-@WebServlet("/firstName")
+@WebServlet("/userName")
 public class UserNamePage extends HttpServlet
 {
 
