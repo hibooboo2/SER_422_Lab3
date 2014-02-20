b238bb29c69ed0eebe047ed720a99cb886463fac
PinkJazz <smoothpinkjazz@gmail.com>
2014-02-19 22:31:55 -0700

    implemented LastNamePage

--- Lab3/src/ser422/lab3/LastNamePage.java
+++ Lab3/src/ser422/lab3/LastNamePage.java
@@ -1,6 +1,10 @@
 package ser422.lab3;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,15 +29,43 @@ public class LastNamePage extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		response.addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.addHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", -1);
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		try
+		{
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Enter your last name</title>");
+			out.println("<body bgcolor=\"pink\"><form method=\"post\">");
+			out.println("<h2>Your last name</h2>");
+			out.println("Last name: <input type=\"text\" name=\"lastname\">");
+			out.println("<input type=\"button\" name=\"prev\" value=\"Previous Page\">");
+			out.println("<input type=\"submit\" name=\"next\" value=\"Next\"> default");
+			out.println("</form></body>");
+			out.println("</html>");
+		}
+		finally
+		{
+			out.close();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		ServletContext sc= getServletContext();
+		RequestDispatcher rd= sc.getRequestDispatcher("/LangsPage");
+		rd.forward(request, response);
 	}
 
 }
